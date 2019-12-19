d3f4fea77a7ef5d16e502364439047d86ff8f0d5
Christopher Dent <christopherdent01@gmail.com>
2019-12-19 23:28:21 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,4 @@
+
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,21 +7,48 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save 
+  redirect to "pets/#{@pet.id}"
+   
+    
+  end
+  
+  
+   get '/pets/:id/edit' do 
+     
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+  
+    erb :'/pets/edit'
   end
 
+ 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
-
+  patch '/pets/:id' do 
+    @owners = Owner.all
+    if !params[:pet].keys.include?("owner_ids")
+      params[:pet]["owner_ids"] = []
+    end  
+     @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owners << Owner.create(name: params["owner"]["name"])
+    end
+    
     redirect to "pets/#{@pet.id}"
   end
 end
