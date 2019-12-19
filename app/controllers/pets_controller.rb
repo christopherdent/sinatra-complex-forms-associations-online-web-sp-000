512b2f22b997d233e2e604026f15126093c4a10f
Christopher Dent <christopherdent01@gmail.com>
2019-12-19 23:19:01 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,15 +13,13 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-  @owners = Owner.all
-  @pet = Pet.create(params[:pet])
-  if !params["owner"]["name"].empty?
-    @pet.owner << Owner.create(name: params["owner"]["name"])
-  end
-  @pet.save 
-  
-   binding.pry 
-    redirect to "pets/#{@pet.id}"
+    @owners = Owner.all
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save 
+  redirect to "pets/#{@pet.id}"
    
     
   end
