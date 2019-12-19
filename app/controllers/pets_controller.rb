73b2937833dd27dcd459cc9e531f092a7e2a884d
Christopher Dent <christopherdent01@gmail.com>
2019-12-19 23:05:18 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,6 +14,7 @@ class PetsController < ApplicationController
 
   post '/pets' do 
   @pet = Pet.create(params[:pet])
+  binding.pry 
   if !params["owner"]["name"].empty?
     @pet.owners << Owner.create(name: params["owner"]["name"])
   end 
